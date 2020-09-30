8709ae401d240468f1df8401073bda357c237a73
wolfbiter <wolfbiter@gmail.com>
2020-09-30 09:19:01 -0700

    improve export to allow for multiple simultaneous exports

--- download-and-add-to-itunes.sh
+++ download-and-add-to-itunes.sh
@@ -4,21 +4,21 @@
 set -e
 
 cd "$(dirname "$0")"
+TIME_STAMP="$(date +%s%3N)"
 
 # parse args
 : ${1?' You forgot to supply a YOUTUBE_URL'}
 : ${2?' You forgot to supply a PLAYLIST_NAME'}
 YOUTUBE_URL=$1
 PLAYLIST_NAME=$2
-WORKING_DIR=${3:-'export'}
+WORKING_DIR=${3:-"export/$TIME_STAMP"}
 
 printf "STARTING SCRIPT: $(basename "$0") || CURRENT TIME: %s\n" "$(date)"
 echo "YOUTUBE_URL: $YOUTUBE_URL"
 echo "PLAYLIST_NAME: $PLAYLIST_NAME"
 
-# reset working directory
-if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
-mkdir $WORKING_DIR
+# create working directory
+mkdir -p $WORKING_DIR
 
 # download from youtube
 cd $WORKING_DIR
@@ -28,7 +28,7 @@ then
 else
   printf "\nERROR DOWNLOADING SOME TRACK(S)\n\n"
 fi
-cd "../"
+cd "../../"
 
 # analyze tracks with keyfinder
 for file in "$WORKING_DIR"/*
@@ -44,4 +44,8 @@ done
 echo "DONE ANALYZING"
 
 # add tracks to itunes
-./create-itunes-playlist.scpt "$(find `pwd` -name $WORKING_DIR)" "$PLAYLIST_NAME"
+echo "$(find `pwd` -name $TIME_STAMP)"
+./create-itunes-playlist.scpt "$(find `pwd` -name $TIME_STAMP)" "$PLAYLIST_NAME"
+
+# clear working directory
+if [ -d "$WORKING_DIR" ]; then rm -rf $WORKING_DIR; fi
