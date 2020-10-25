89d8b378567f2c7cbd383f1bcb799b29c670ddd0
wolfbiter <wolfbiter@gmail.com>
2020-10-25 12:20:02 -0700

    WIP: got tracks importing with offset but still need to rearrange them

---
+++ audacity/pipe_client.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+TOFILE = None
+FROMFILE = None
+EOL = None
+
+def load_track(filename, start=0, end=None, track=None):
+  assert track != None
+  assert end != None
+  do( f'Import2: Filename="{filename}"' )
+
+  # reset cursor to start of track
+  trackString = f'Track={track}'
+  do( f'SelectTracks: {trackString}')
+  do( 'CursTrackStart' )
+
+  # trim track to start / end
+  startString = f'Start={start}' if start != None else ''
+  endString = f'End={end}' if end != None else ''
+  do( f'Select: {startString} {endString} {trackString}')
+  do( 'Trim' )
+  do( 'ZoomSel' )
+
+def send_command(command):
+  """Send a single command."""
+  global TOFILE
+  global EOL
+  TOFILE.write(command + EOL)
+  TOFILE.flush()
+
+def get_response():
+  """Return the command response."""
+  global FROMFILE
+  result = ''
+  line = ''
+  while True:
+    result += line
+    line = FROMFILE.readline()
+    if line == '\n' and len(result) > 0:
+      break
+      return result
+
+def do_command(command):
+  """Send one command, and return the response."""
+  assert_pipes()
+  # print("Send: >>> \n"+command)
+  send_command(command)
+  response = get_response()
+  # print("Rcvd: <<< \n" + response)
+  return response
+
+def do(command):
+  do_command(command)
+
+def assert_pipes():
+  if not (TOFILE and FROMFILE):
+    get_pipes()
+
+def get_pipes():
+  global TOFILE
+  global FROMFILE
+  global EOL
+  if sys.platform == 'win32':
+      TONAME = '\\\\.\\pipe\\ToSrvPipe'
+      FROMNAME = '\\\\.\\pipe\\FromSrvPipe'
+      EOL = '\r\n\0'
+  else:
+      TONAME = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
+      FROMNAME = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+      EOL = '\n'
+
+  TOFILE = open(TONAME, 'w')
+  FROMFILE = open(FROMNAME, 'rt')
+
+
+def close_pipes():
+  global TOFILE
+  global FROMFILE
+
+  if TOFILE:
+    TOFILE.close()
+    TOFILE = None
+
+  if FROMFILE:
+    FROMFILE.close()
+    FROMFILE = None
--- export-playlist-to-audacity.py
+++ export-playlist-to-audacity.py
@@ -1,9 +1,12 @@
 import argparse
 import subprocess
+import math
 
 from os import listdir
 from os.path import isfile, join, basename
 from pathlib import Path
+
+import audacity.pipe_client as audacity
 
 ap = argparse.ArgumentParser()
 ap.add_argument('playlist_path', metavar='playlist_path', type=str, help="path to playlist folder to convert, .m3u file in a flat folder with audio files")
@@ -13,6 +16,26 @@ PLAYLIST_PATH = '/Users/kane/Desktop/panako-input' or flags.playlist_path
 DOCKER_AUDIO_PATH = '/home/audioinput'
 DOCKER_WORKING_DIRECTORY = '/Users/kane/projects/docker-panako'
 DOCKER_COMMAND = f'docker run -i --volume {PLAYLIST_PATH}:{DOCKER_AUDIO_PATH} --rm panako bash'
+
+
+def add_transitions_to_audacity(transitions):
+  for i, transition in enumerate(transitions):
+    print('add_transition_to_audacity', transition)
+
+    # only load x for first transition
+    if i == 0:
+      audacity.load_track(transition['x'], start=0, end=transition['offset'], track=i)
+
+    # get y_end from next transition's offset
+    y_end = 100
+    if i != len(transitions) - 1:
+      next_transition = transitions[i + 1]
+      y_end = next_transition['offset']
+
+    # load y
+    audacity.load_track(transition['y'], start=transition['offset'], end=y_end, track=i)
+
+  audacity.close_pipes()
 
 
 def sync_pair(x, y):
@@ -31,8 +54,10 @@ def sync_pair(x, y):
     cwd=DOCKER_WORKING_DIRECTORY)
 
   # run sync command
+  xPath = join(DOCKER_AUDIO_PATH, basename(x))
+  yPath = join(DOCKER_AUDIO_PATH, basename(y))
   stdout, stderr = process.communicate(
-    input=f'panako sync SYNC_MIN_ALIGNED_MATCHES=1 "{x}" "{y}"')
+    input=f'panako sync SYNC_MIN_ALIGNED_MATCHES=1 "{xPath}" "{yPath}"')
   if stderr:
     raise Exception(stderr)
 
@@ -51,6 +76,7 @@ def sync_pair(x, y):
 
 
 def main():
+  # TODO: ensure audacity is running first?
   print(f'PYTHON EXPORT PLAYLIST_PATH: {PLAYLIST_PATH}')
 
   # find .m3u
@@ -63,22 +89,22 @@ def main():
   # parse audio files from .m3u
   m3u_path = join(PLAYLIST_PATH, m3u_file)
   m3u_lines = open(m3u_path, 'r').read().split('\n')
-  audio_files = [join(DOCKER_AUDIO_PATH, basename(f))
-    for f in m3u_lines if f]
+  audio_files = [join(PLAYLIST_PATH, basename(f)) for f in m3u_lines if f]
 
-  # compare audio file by file
-  pairs = []
+  # collect audio files into transition pairs
+  transitions = []
   for i in range(len(audio_files) - 1):
     x = audio_files[i]
     y = audio_files[i + 1]
-    pairs.append([x, y])
+    transitions.append({ 'x': x, 'y': y, 'offset': math.inf })
 
-  offsets = []
-  for i, [x, y] in enumerate(pairs):
-    offset = sync_pair(x, y)
-    offsets.append(offset)
+  # sync transition pairs in panako
+  transitions = transitions[:1]
+  for transition in transitions:
+    transition['offset'] = sync_pair(transition['x'], transition['y'])
 
-  print('\nSynced pairs')
+  # add synced transitions to audacity project
+  add_transitions_to_audacity(transitions)
 
 
 if __name__ == '__main__':
